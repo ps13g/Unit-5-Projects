8d0735dee25a5e2415abfe56016475f72fc98646
ps13g <saketh.2004@gmail.com>
2022-11-14 12:25:43 -0700

    Fraction assignment

--- src/FractionTester.java
+++ src/FractionTester.java
@@ -3,11 +3,15 @@ public class FractionTester {
         Fraction first = new Fraction(1,2);
         Fraction second = new Fraction(1,3);
         System.out.println();
+
         System.out.println("BEFORE:");
         System.out.println("first: "+first);
         System.out.println("second: "+second);
+
         first.multiply(second);
+
         System.out.println("AFTER:");
+
         System.out.println("first: "+first);
         System.out.println("second: "+second);
     }
