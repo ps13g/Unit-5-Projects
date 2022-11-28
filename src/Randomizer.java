8e4726bf8be9b9537bca215c6572884d96054ca5
ps13g <saketh.2004@gmail.com>
2022-11-28 12:12:38 -0700

    Fixing randomizer class

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -3,7 +3,7 @@ public class Randomizer
     public static int nextInt()
     {
         //Implement this method to return a random number from 1-10
-        return (int)(Math.random() * (9 + 1) + 1);
+        return (int)(Math.random() * (10) + 1);
     }
 
     public static int nextInt(int min, int max)
